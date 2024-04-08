b826e1f1a46cf5700b6207a84477270387551a6e
Robin Schulz <hello@rschu.me>
2024-04-08 05:13:30 +0200

    init commit with medium blog scraper

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # python-playground
+
 My Git repository for tinkering with Python – from simple scripts to fun mini-projects.
+
+## Table of Contents
+* [Prerequisites](#prerequisites)
+* [Medium Blog Scraper](#medium-blog-scraper-extract-titles-links-and-publishing-dates)
+  * [Valid Medium Blog URL and content](#valid-medium-blog-url-and-content)
+  * [Invalid Medium Blog URL](#invalid-medium-blog-url)
+  * [Invalid URL](#invalid-url)
+* [License](#license)
+
+
+## Prerequisites
+This project's dependencies are managed by Poetry. See the `pyproject.toml` file for a complete list.
+
+* **Poetry:** 1.8.2 or later. See installation instructions at https://python-poetry.org/docs/#installation
+* **Python:** 3.12 or later.
+
+
+## Medium Blog Scraper: Extract Titles, Links, and Publishing Dates
+A simple Python script for scraping and compiling a list of article metadata from any Medium blog. By utilizing Selenium and BeautifulSoup to navigate and parse articles through infinite scroll pages, we extract titles, links, and publication dates and store this information in comma-separated text files (e.g., `blog_articles_rschu.me.csv`).
+
+Running the script `python python-playground/medium_blog.py` will prompt for any Medium blog URL, including the discontinued custom Medium Blog domains. 
+
+### Valid Medium Blog URL and content
+```bash
+Enter the Medium Blog URL (e.g., https://rschu.me): https://rschu.me
+Processing Blog articles from: https://rschu.me
+Found 113 articles so far [-]
+
+------------------
+113 Articles found
+------------------
+
+Example:
+Title:  Unlock the ability to place bets using Twitch Channel Points for chat predictions.
+Link:  https://rschu.me/unlock-placing-bets-with-twitch-channel-points-for-chat-predictions-783c2eadeab8
+Published at:  Dec 28, 2021
+
+CSV file created: .../blog_articles_rschu.me.csv
+```
+
+### Invalid Medium Blog URL
+```bash
+Enter the Medium Blog URL (e.g., https://rschu.me): https://google.com
+Processing Blog articles from: https://google.com
+'https://google.com' does not seem to be a Medium blog.
+```
+
+### Invalid URL
+```bash
+Enter the Medium Blog URL (e.g., https://rschu.me): https://rschume
+The given URL 'https://rschume' appears to be invalid.
+```
+
+## License
+This project is licensed under the [MIT License](LICENSE) - see the LICENSE file for details.
---
+++ python-playground/medium_blog.py
@@ -0,0 +1,173 @@
+import csv
+import os
+import re
+import time
+from typing import Any
+from urllib.parse import urljoin, urlparse
+
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+from selenium import webdriver
+
+ua: Any = UserAgent()
+
+
+def is_valid_url(url):
+    # Check for a valid scheme and netloc.
+    parsed_url = urlparse(url)
+    if not parsed_url.scheme or not parsed_url.netloc:
+        return False
+
+    # Regular expression for validating a URL.
+    regex = re.compile(
+        r"^(?:http|ftp)s?://"
+        r"(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)"  # noga
+        r"(?:/?|[/?]\S+)$",
+        re.IGNORECASE,
+    )
+
+    return re.match(regex, url) is not None
+
+
+def spinner():
+    while True:
+        for character in "|/-\\":
+            yield character
+
+
+def main():
+    # ----- Site configuration -----
+    scroll_pause_time = 2
+    scrolls_before_check = 2
+    default_blog_url = "https://rschu.me"
+    blog_url = input(f"Enter the Medium Blog URL (e.g., {default_blog_url}): ").strip("/") or default_blog_url
+
+    if not is_valid_url(blog_url):
+        print(
+            f"The given URL '{blog_url}' appears to be invalid.", end="", flush=True
+        )
+        exit(0)
+
+    spin = spinner()
+
+    print(f"Processing Blog articles from: {blog_url}")
+
+    # ----- Webdriver setup -----
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("window-size=1920x1080")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument(f"user-agent={ua.random}")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option("useAutomationExtension", False)
+    driver = webdriver.Chrome(options=options)
+    driver.get(blog_url)
+
+    # ----- Scrolling and data extraction -----
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    articles_data = []
+
+    while True:
+        # Scroll to the bottom of the page.
+        for _ in range(scrolls_before_check):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(scroll_pause_time)
+            print(
+                f"\rFound {len(articles_data)} articles so far [{next(spin)}]",
+                end="",
+                flush=True,
+            )
+
+        html_content = driver.page_source
+        soup = BeautifulSoup(html_content, "html.parser")
+
+        is_medium_url = bool(soup.find("meta", {"content": "com.medium.reader"}))
+        if not is_medium_url:
+            print(
+                f"\r'{blog_url}' does not appear to be a Medium blog.",
+                end="",
+                flush=True,
+            )
+            exit(0)
+
+        # Check if we've reached the end of the page.
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+        # Extract articles from the page.
+        articles = soup.find_all("article")
+        for article in articles:
+
+            print(
+                f"\rFound {len(articles_data)} articles so far [{next(spin)}]",
+                end="",
+                flush=True,
+            )
+
+            link_url = ""
+            publishing_date = "not-found"
+            links = article.find_all("a")
+
+            # Find the primary article link within the 'article' block.
+            for link in links:
+                href = link.get("href")
+                if href and href.startswith("/"):
+                    parsed_url = urlparse(href)
+                    full_url = urljoin(blog_url, parsed_url.path)
+                    link_url = full_url
+                    break
+
+            date_pattern = re.compile(
+                r"\bJan|Feb|Mar|Apr|May|Jun|Jul|"
+                r"Aug|Sep|Oct|Nov|Dec\b \d{1,2}, \d{4}"
+            )
+            date_tag = article.find(string=date_pattern)
+            if date_tag:
+                publishing_date = date_tag
+
+            title = article.find("h2")
+            if title:
+                articles_data.append(
+                    {
+                        "title": title.text,
+                        "link": link_url,
+                        "published": publishing_date,
+                    }
+                )
+
+    # ----- Results -----
+    result_text = f"{len(articles_data)} Articles found"
+    separator_line = "-" * len(result_text)
+    print(f"\n\n{separator_line}")
+    print(result_text)
+    print(f"{separator_line}")
+    if len(articles_data) > 0:
+        print("\nExample:")
+        print("Title: ", articles_data[0]["title"])
+        print("Link: ", articles_data[0]["link"])
+        print("Published at: ", articles_data[0]["published"])
+
+    try:
+        # Save the result to a CSV file.
+        domain_name = urlparse(blog_url).netloc
+        csv_filename = f"blog_articles_{domain_name}.csv"
+
+        with open(csv_filename, "w", newline="") as csvfile:
+            fieldnames = ["title", "link", "published"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(articles_data)
+
+            print(f"\nCSV file created: {os.path.abspath(csv_filename)}")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+    # Close the browser.
+    driver.quit()
+
+
+if __name__ == "__main__":
+    main()
